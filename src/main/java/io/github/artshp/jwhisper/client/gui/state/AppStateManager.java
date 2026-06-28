9bf7d19ec5564010cf894e64ad2086c830063003
Artemiy Shipovalov <56469391+ArtShp@users.noreply.github.com>
2026-06-28 13:55:05 +0200

    Add support of message listeners

--- src/main/java/io/github/artshp/jwhisper/client/gui/state/AppStateManager.java
+++ src/main/java/io/github/artshp/jwhisper/client/gui/state/AppStateManager.java
@@ -2,9 +2,14 @@ package io.github.artshp.jwhisper.client.gui.state;
 
 import io.github.artshp.jwhisper.client.gui.config.ClientConfig;
 import io.github.artshp.jwhisper.client.gui.config.ConfigManager;
+import io.github.artshp.jwhisper.client.gui.event.MessageListener;
 import io.github.artshp.jwhisper.client.gui.users.UserKeys;
+import javafx.application.Platform;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * Class responsible for application state.
@@ -15,6 +20,11 @@ public class AppStateManager {
      * Application config manager
      */
     private final ConfigManager configManager = new ConfigManager();
+
+    /**
+     * Incoming messages listeners
+     */
+    private final List<MessageListener> messageListeners = new CopyOnWriteArrayList<>();
 
     /**
      * Current username
@@ -69,5 +79,28 @@ public class AppStateManager {
         configManager.saveConfig(config);
 
         this.serverPort = port;
+    }
+
+    /**
+     * Add messages listener to the list to be notified about incoming messages.
+     * @param listener listener to be registered
+     * @see #dispatchMessage(String, String)
+     */
+    public void addMessageListener(MessageListener listener) {
+        messageListeners.add(listener);
+    }
+
+    /**
+     * Handle incoming message event, notify listeners.
+     * @param sender sender username
+     * @param message message text
+     * @see #addMessageListener(MessageListener)
+     */
+    public void dispatchMessage(String sender, String message) {
+        Platform.runLater(() -> {
+            for (MessageListener listener : messageListeners) {
+                listener.onMessageReceived(sender, message);
+            }
+        });
     }
 }
