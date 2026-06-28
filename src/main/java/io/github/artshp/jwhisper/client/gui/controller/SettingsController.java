4b16ea2e69631f5140ec2ece8c914c61293a4de2
Artemiy Shipovalov <56469391+ArtShp@users.noreply.github.com>
2026-06-28 20:00:38 +0200

    Add option to read certificate from file

--- src/main/java/io/github/artshp/jwhisper/client/gui/controller/SettingsController.java
+++ src/main/java/io/github/artshp/jwhisper/client/gui/controller/SettingsController.java
@@ -3,13 +3,20 @@ package io.github.artshp.jwhisper.client.gui.controller;
 import io.github.artshp.jwhisper.client.gui.navigation.SceneSwitcher;
 import io.github.artshp.jwhisper.client.gui.security.ServerTrustManager;
 import io.github.artshp.jwhisper.client.gui.state.AppStateManager;
+import io.github.artshp.jwhisper.client.gui.util.DialogFactory;
 import io.github.artshp.jwhisper.common.crypto.CertUtils;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.security.cert.X509Certificate;
 import java.util.Optional;
 
@@ -40,6 +47,12 @@ public class SettingsController {
      */
     @FXML
     private TextField portField;
+
+    /**
+     * Path or name of selected certificate file
+     */
+    @FXML
+    private TextField filePathField;
 
     /**
      * Certificate field
@@ -73,6 +86,38 @@ public class SettingsController {
     }
 
     /**
+     * Handle "browse certificate" operation. Read certificate from file and parse it.
+     */
+    @FXML
+    private void handleBrowseCertificateFile() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select server certificate");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Certificate Files", "*.crt", "*.pem", "*.cer"),
+                new FileChooser.ExtensionFilter("All Files", "*.*")
+        );
+
+        Stage currentStage = (Stage) filePathField.getScene().getWindow();
+        File selectedFile = fileChooser.showOpenDialog(currentStage);
+
+        if (selectedFile != null) {
+            try {
+                filePathField.setText(selectedFile.getName());
+
+                String pemContent = Files.readString(selectedFile.toPath());
+                pemCertificateField.setText(pemContent);
+            } catch (IOException e) {
+                DialogFactory.createAlert(
+                        Alert.AlertType.ERROR,
+                        "File read error",
+                        "Could not process certificate asset file",
+                        e.getMessage()
+                ).showAndWait();
+            }
+        }
+    }
+
+    /**
      * Handle "import certificate" operation.
      * Tries to import provided certificate to truststore.
      * @see ServerTrustManager
@@ -84,11 +129,29 @@ public class SettingsController {
         if (pemString == null || pemString.isBlank() || password.isBlank()) return;
 
         char[] charPassword = password.toCharArray();
-        ServerTrustManager serverTrustManager = new ServerTrustManager(charPassword);
+        ServerTrustManager serverTrustManager;
+        try {
+            serverTrustManager = new ServerTrustManager(charPassword);
+        } catch (Exception e) {
+            DialogFactory.createAlert(
+                    Alert.AlertType.ERROR,
+                    "Import failed",
+                    "Failed to open truststore",
+                    e.getMessage()
+            ).showAndWait();
+            return;
+        }
 
         Optional<X509Certificate> certificateOptional = CertUtils.parsePemCertificate(pemString);
         if (certificateOptional.isEmpty()) {
             LOGGER.error("Failed to load certificate.");
+
+            DialogFactory.createAlert(
+                    Alert.AlertType.ERROR,
+                    "Import failed",
+                    null,
+                    "Provided certificate is not a valid certificate."
+            ).showAndWait();
             return;
         }
 
@@ -96,7 +159,15 @@ public class SettingsController {
         serverTrustManager.addTrustedCertificate(certificate);
 
         pemCertificateField.clear();
+        filePathField.clear();
         passwordField.clear();
+
+        DialogFactory.createAlert(
+                Alert.AlertType.INFORMATION,
+                "Import successful",
+                null,
+                "The certificate was successfully added to truststore."
+        ).showAndWait();
     }
 
     /**
